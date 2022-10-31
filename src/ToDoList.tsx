67a2ca2d6670c840340faf37b14b162957482685
ban <halfmin@gmail.com>
2022-10-31 15:16:57 +0900

    UserAddForm add & toDoList setting

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,46 +1,29 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  toDo: string;
+}
 
 function ToDoList() {
-  const { register, watch, handleSubmit } = useForm();
-  const onValid = (data: any) => {
-    console.log(data);
-  };
+  const {
+    register, handleSubmit, setValue
+  } = useForm<IForm>();
+  const onSubmit = (data: IForm) => {
+    console.log('add to do', data.toDo);
+    setValue("toDo", "");
+  }
   return (
     <>
       <div>
-        <form onSubmit={handleSubmit(onValid)}>
-          <input {...register("toDo")} placeholder="Write a to do" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("toDo", { required: "Please write a To Do", })} placeholder="Write a to do" />
           <button>Add</button>
         </form>
       </div>
     </>
   );
 }
-/*
-function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value }
-    } = event;
-    setToDo(value);
-    //console.log(event, event.currentTarget, event.target, value);
-
-  }
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  }
-  return (
-    <>
-      <div>
-        <form onSubmit={onSubmit}>
-          <input onChange={onChange} value={toDo} placeholder="Write a to do" />
-          <button>Add</button>
-        </form>
-      </div>
-    </>
-  );
-}
-*/
 export default ToDoList;
+
+
