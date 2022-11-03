445aae70fff8c73ecb06b89e1f045f529fad9b80
ban <halfmin@gmail.com>
2022-11-03 17:29:45 +0900

    Recording

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,19 +1,7 @@
 import { atom, selector } from "recoil";
 
-export const minuteState = atom({
-  key: "minutes",
-  default: 0
+export const toDoState = atom({
+  key: "toDo",
+  default: ["a", "b", "c", "d", "e", "f"],
 });
 
-export const hourSelector = selector<number>({
-  key: "hours",
-  get: ({ get }) => {
-    const minutes = get(minuteState);
-    return minutes / 60;
-  },
-  set: ({ set }, newValue) => {
-    console.log(newValue);
-    const minutes = Number(newValue) * 60;
-    set(minuteState, minutes);
-  }
-}); 
