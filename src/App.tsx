109fc7920d281690d4acae9371b995cfaf20d62e
ban <halfmin@gmail.com>
2022-11-15 15:38:48 +0900

    #8.10 SVG Animation (12:28)

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,97 @@
-import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
-import { useRecoilState } from "recoil";
-import styled from "styled-components";
-import { toDoState } from "./atoms";
-import Board from "./Components/Board";
-import DragabbleCard from "./Components/DragabbleCard";
+import styled, { ThemeProvider } from "styled-components";
+import { motion, useMotionValue, useTransform, useViewportScroll } from "framer-motion";
+import { darkTheme } from "./theme";
+import { useEffect, useRef } from "react";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
   display: flex;
-  max-width: 680px;
-  width: 100%;
-  margin: 0 auto;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  // background-color : ${(props) => props.theme.bgColor}}
+  background : linear-gradient(135deg, rgb(238,0,153), rgb(221,0,238))
+`;
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Boards = styled.div`
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
   display: grid;
-  width: 100%;
-  gap: 10px;
-  grid-template-columns: repeat(3, 1fr);
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-
-
-function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  //const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
-  const onDragEnd = (info: DropResult) => {
-    console.log(info);
-    const { destination, draggableId, source } = info;
-    if (!destination) return;
-    if (destination?.droppableId === source.droppableId) {
-      // same board movement.
-      setToDos((allBoards) => {
-        const boardCopy = [...allBoards[source.droppableId]];
-        const taskObj = boardCopy[source.index];
-        boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: boardCopy,
-        };
-      });
-    }
-    if (destination?.droppableId !== source.droppableId) {
-      // cross board movement
-      setToDos((allBoards) => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const taskObj = sourceBoard[source.index];
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
+const boxVariants = {
+  hover: { rotateZ: 90 },
+  click: { borderRadius: "100px" },
+};
+const Svg = styled.svg`
+  width : 300px;
+  height : 300px;
+  color : white;
+  path {
+    stroke:white;
+    storke-width : 2;
   }
+`;
+const svg = {
+  start: {
+    pathLength: 0,
+    fill: "rgba(255,255,255,0)",
+  },
+  end: {
+    pathLength: 1,
+    fill: "rgba(255,255,255,1)",
+
+  }
+}
+function App() {
+  const x = useMotionValue<number>(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0,210,238), rgb(0,83,238))",
+      "linear-gradient(135deg, rgb(0,238,153), rgb(238,178,0))"
+    ]
+  );
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  useEffect(() => {
+    //x.onChange(() => console.log("x", x.get()));
+    rotateZ.onChange(() => console.log("scale", rotateZ.get()));
+
+  }, [x]);
+
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Wrapper>
-        <Boards>
-          {Object.keys(toDos).map((boardId) => (
-            <Board key={boardId} boardId={boardId} toDos={toDos[boardId]} />
-          ))}
-        </Boards>
+    <ThemeProvider theme={darkTheme}>
+      <Wrapper style={{ background: gradient }}>
+        <Svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512">
+          <motion.path
+            variants={svg}
+            initial="start"
+            animate="end"
+            transition={{
+              default: { duration: 5 },
+              fill: { duration: 2, delay: 3 }
+            }}
+            stroke="white"
+            strokeWidth="2"
+            d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z" /></Svg>
       </Wrapper>
-    </DragDropContext>
+    </ThemeProvider>
   );
 }
 
