332fe4cbf497fed9f55791741d20b51b61b774a2
ban <halfmin@gmail.com>
2022-11-18 14:06:58 +0900

    slide add

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,10 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMovesResult } from "../api";
-import { makeImagePath } from "./utilities";
+import useWindowDimensions from "../useWidowDimensions";
+import { makeImagePath } from "../utilities";
 
 const Wrapper = styled.div`
   background:black;
@@ -28,21 +31,99 @@ const Title = styled.h2`
 const Overview = styled.p`
   font-size:30px;
 `
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+  
+`;
+
+const Box = styled(motion.div) <{ bgPhoto: string }>`
+  background-color: white;
+  background-image:url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
+  height: 200px;
+  font-size: 66px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 5,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 5,
+  },
+};
+
+const offset = 6;
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMovesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
-  console.log(data, isLoading)
+  const [index, setIndex] = useState(0);
+  const incraseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data?.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  }
+  const [leaving, setLeaving] = useState(false);
+  const toggleLeaving = () => setLeaving((prev) => !prev);
   return <Wrapper>
-    {isLoading ? <Loader>Loading...</Loader>
-      : <>
-        <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+    {isLoading ? (
+      <Loader>Loading...</Loader>
+    ) : (
+      <>
+        <Banner
+          onClick={incraseIndex}
+          bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+        >
           <Title>{data?.results[0].title}</Title>
           <Overview>{data?.results[0].overview}</Overview>
         </Banner>
+        <Slider>
+          <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+            <Row
+              variants={rowVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              transition={{ type: "tween", duration: 1 }}
+              key={index}
+            >
+              {data?.results
+                .slice(1)
+                .slice(offset * index, offset * index + offset)
+                .map((movie) => (
+                  <Box
+                    key={movie.id}
+                    bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                  >
+
+                  </Box>
+                ))}
+            </Row>
+          </AnimatePresence>
+        </Slider>
       </>
-    }</Wrapper>;
+    )}
+  </Wrapper>;
 }
 
 export default Home;
