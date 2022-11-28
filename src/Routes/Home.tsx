fc8487047f24b00e008063ec7d57f5542432a545
ban <halfmin@gmail.com>
2022-11-28 15:19:28 +0900

    slider part 2

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -15,13 +15,13 @@ const Loader = styled.div`
   justify-content:center;
   align-items:center;
 `;
-const Banner = styled.div<{ bgPhoto: string }>`
+const Banner = styled.div<{ bgphoto: string }>`
   height:100vh;
   display:flex;
   flex-direction:column;
   justify-content:center;
   padding:60px;
-  background-image:linear-gradient(rgba(0,0,0,0),rgba(0,0,0,1)),url(${props => props.bgPhoto});
+  background-image:linear-gradient(rgba(0,0,0,0),rgba(0,0,0,1)),url(${props => props.bgphoto});
   background-size:cover;
 `
 const Title = styled.h2`
@@ -45,9 +45,9 @@ const Row = styled(motion.div)`
   
 `;
 
-const Box = styled(motion.div) <{ bgPhoto: string }>`
+const Box = styled(motion.div) <{ bgphoto: string }>`
   background-color: white;
-  background-image:url(${(props) => props.bgPhoto});
+  background-image:url(${(props) => props.bgphoto});
   background-size: cover;
   background-position: center center;
   height: 200px;
@@ -92,7 +92,7 @@ function Home() {
       <>
         <Banner
           onClick={incraseIndex}
-          bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          bgphoto={makeImagePath(data?.results[0].backdrop_path || "")}
         >
           <Title>{data?.results[0].title}</Title>
           <Overview>{data?.results[0].overview}</Overview>
@@ -113,7 +113,9 @@ function Home() {
                 .map((movie) => (
                   <Box
                     key={movie.id}
-                    bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                    whileHover={{ scale: 1.3 }}
+                    transition={{ delay: 2 }}
+                    bgphoto={makeImagePath(movie.backdrop_path, "w500")}
                   >
 
                   </Box>
